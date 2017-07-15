bf0e1fd3067ec22f04792313ea8ffc6fdbbae015
Avelino <t@avelino.xxx>
2017-07-15 09:36:58 -0300

    migrate squirrel to dut language

--- dut-mode.el
+++ dut-mode.el
@@ -1,82 +1,83 @@
-;;; squirrel-mode.el --- mode for editing Squirrel code
-
-;; Author: Hideaki Takei
-;; Created: Dec 2008
-;; Keywords: languages
+;;; dut-mode.el --- Major mode for the Dut programming language
 
 ;;; Commentary:
 
-;; Provides fairly minimal font-lock and indentation support for
-;; editing Squirrel code.
+;; Copyright 2017 The dut-mode Authors.  All rights reserved.
+;; Use of this source code is governed by a MIT
+;; license that can be found in the LICENSE file.
 
+;; Author: The dut-mode Authors
+;; Version: 1.0
+;; Keywords: languages gut
+;; URL: https://github.com/dut-lang/dut-mode
+;;
+;; This file is not part of GNU Emacs.
 
 ;;; Code:
 
+(add-to-list 'auto-mode-alist '("\\.dut\\'" . dut-mode))
 
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.nut\\'" . squirrel-mode))
+(defvar dut-mode-hook nil)
 
-(defvar squirrel-mode-hook nil)
+(defvar dut-mode-map
+  (let ((dut-mode-map (make-sparse-keymap)))
+    (define-key dut-mode-map "{" 'dut-electric-brace)
+    (define-key dut-mode-map "}" 'dut-electric-brace)
+    (define-key dut-mode-map ":" 'dut-electric-colon)
+    (define-key dut-mode-map "\C-c\C-o" 'dut-insert-block)
+    dut-mode-map)
+  "Keymap for dut major mode")
 
-(defvar squirrel-mode-map
-  (let ((squirrel-mode-map (make-sparse-keymap)))
-    (define-key squirrel-mode-map "{" 'squirrel-electric-brace)
-    (define-key squirrel-mode-map "}" 'squirrel-electric-brace)
-    (define-key squirrel-mode-map ":" 'squirrel-electric-colon)
-    (define-key squirrel-mode-map "\C-c\C-o" 'squirrel-insert-block)
-    squirrel-mode-map)
-  "Keymap for Squirrel major mode")
+(defvar dut-indent-level 4
+  "Number of columns for a unit of indentation in dut mode.")
 
-(defvar squirrel-indent-level 4
-  "Number of columns for a unit of indentation in Squirrel mode.")
-
-(defconst squirrel-font-lock-keywords
+(defconst dut-font-lock-keywords
   (list
    '("\\<\\(base\\|break\\|case\\|catch\\|clone\\|continue\\|const\\|default\\|delete\\|else\\|enum\\|extends\\|for\\|foreach\\|function\\|if\\|in\\|local\\|null\\|resume\\|return\\|switch\\|this\\|throw\\|try\\|typeof\\|while\\|yield\\|constructor\\|instanceof\\|true\\|false\\|static\\)\\>" . font-lock-builtin-face)
    '("\\<\\(class\\)\\s-+\\(\\w+\\)\\>" (1 font-lock-keyword-face) (2 font-lock-type-face))
    '("\\<\\(function\\)\\s-+\\(\\w+\\)\\>" (1 font-lock-keyword-face) (2 font-lock-function-name-face))
    ))
 
-(defvar squirrel-mode-syntax-table
-  (let ((squirrel-mode-syntax-table (make-syntax-table)))
-    (modify-syntax-entry ?_ "w" squirrel-mode-syntax-table)
-    (modify-syntax-entry ?/ ". 124b" squirrel-mode-syntax-table)
-    (modify-syntax-entry ?* ". 23" squirrel-mode-syntax-table)
-    (modify-syntax-entry ?\n "> b" squirrel-mode-syntax-table)
-    squirrel-mode-syntax-table)
-  "Syntax table for squirrel-mode")
+(defvar dut-mode-syntax-table
+  (let ((dut-mode-syntax-table (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" dut-mode-syntax-table)
+    (modify-syntax-entry ?/ ". 124b" dut-mode-syntax-table)
+    (modify-syntax-entry ?* ". 23" dut-mode-syntax-table)
+    (modify-syntax-entry ?\n "> b" dut-mode-syntax-table)
+    dut-mode-syntax-table)
+  "Syntax table for dut-mode")
 
 
 
-(defun squirrel-electric-brace (arg)
+(defun dut-electric-brace (arg)
   (interactive "P")
   (insert-char last-command-event 1)
-  (squirrel-indent-line)
+  (dut-indent-line)
   (if (looking-back "^\\s *")
       (forward-char)
     (delete-char -1)
     (self-insert-command (prefix-numeric-value arg))))
 
-(defun squirrel-electric-colon (arg)
+(defun dut-electric-colon (arg)
   (interactive "P")
   (insert-char ?: 1)
-  (squirrel-indent-line))
+  (dut-indent-line))
 
 
-(defun squirrel-insert-block ()
+(defun dut-insert-block ()
   (interactive)
   (insert-char ?{ 1)
-  (squirrel-indent-line)
+  (dut-indent-line)
   (newline)
   (newline)
   (insert-char ?} 1)
-  (squirrel-indent-line)
+  (dut-indent-line)
   (forward-line -1)
-  (squirrel-indent-line))
+  (dut-indent-line))
 
 
-(defun squirrel-indent-line ()
-  "Indent current line as squirrel code"
+(defun dut-indent-line ()
+  "Indent current line as dut code"
   (interactive)
   (let ((pos0 (point)) (pos1 (point)))
     (save-excursion
@@ -84,21 +85,21 @@
       (let ((state (syntax-ppss)) level (indent-old 0) indent-new prev-level)
     (if (nth 8 state)
         () ;; Inside string or comment: do nothing
-      (setq prev-level (squirrel-continuation-indent-level))
+      (setq prev-level (dut-continuation-indent-level))
       (if prev-level
-          (indent-line-to (+ prev-level squirrel-indent-level)) ;; continuation line
+          (indent-line-to (+ prev-level dut-indent-level)) ;; continuation line
         (setq level (nth 0 state)) ;; get nest level
         (if (looking-at "\\s-*\\(}\\|\\]\\|)\\)")
         (setq level (1- level))
           (if (looking-at "\\s-*\\(case\\|default\\)")
           (setq level (1- level))))
-        (indent-line-to (* squirrel-indent-level (max 0 level))))
+        (indent-line-to (* dut-indent-level (max 0 level))))
       (setq pos1 (point)))))
     (if (> pos1 pos0)
     (goto-char pos1))))
 
 
-(defun squirrel-continuation-indent-level ()
+(defun dut-continuation-indent-level ()
   (save-excursion
     (while (forward-comment -1))
     (if (looking-back ")")
@@ -118,23 +119,20 @@
       (if (looking-back "[-+=~!/*%<>^|&?]")
           (current-indentation)))))))
 
-
-
-;;;###autoload
-(defun squirrel-mode ()
-  "Major mode for editing Squirrel script"
+(defun dut-mode ()
+  "Major mode for editing dut script"
   (interactive)
   (kill-all-local-variables)
-  (set-syntax-table squirrel-mode-syntax-table)
-  (use-local-map squirrel-mode-map)
-  (set (make-local-variable 'font-lock-defaults) '(squirrel-font-lock-keywords))
-  (set (make-local-variable 'indent-line-function) 'squirrel-indent-line)
+  (set-syntax-table dut-mode-syntax-table)
+  (use-local-map dut-mode-map)
+  (set (make-local-variable 'font-lock-defaults) '(dut-font-lock-keywords))
+  (set (make-local-variable 'indent-line-function) 'dut-indent-line)
   (set (make-local-variable 'comment-start) "// ")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'parse-sexp-ignore-comments) t)
 
-  (setq major-mode 'squirrel-mode)
-  (setq mode-name "Squirrel")
-  (run-hooks 'squirrel-mode-hook))
+  (setq major-mode 'dut-mode)
+  (setq mode-name "dut")
+  (run-hooks 'dut-mode-hook))
 
-(provide 'squirrel-mode)
+(provide 'dut-mode)
